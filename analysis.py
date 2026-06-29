6c977d0479d62786626f7fd468e3280bc67e8358
gabrieleferrari <ferrari.gabriele1999@gmail.com>
2026-06-29 01:41:38 +0100

    Add max and minimum

--- analysis.py
+++ analysis.py
@@ -14,3 +14,11 @@ with data_file.open(newline="", encoding="utf-8") as file:
 average_rainfall = sum(rainfall_values) / len(rainfall_values)
 
 print(f"Average monthly rainfall: {average_rainfall:.1f} mm")
+
+average_rainfall = sum(rainfall_values) / len(rainfall_values)
+minimum_rainfall = min(rainfall_values)
+maximum_rainfall = max(rainfall_values)
+
+print(f"Average monthly rainfall: {average_rainfall:.1f} mm")
+print(f"Minimum monthly rainfall: {minimum_rainfall:.1f} mm")
+print(f"Maximum monthly rainfall: {maximum_rainfall:.1f} mm")
