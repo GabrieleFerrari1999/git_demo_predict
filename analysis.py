df432eba36426eab86fbaf3b743f05d731a518fe
gabrieleferrari <ferrari.gabriele1999@gmail.com>
2026-06-29 10:27:55 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -12,6 +12,7 @@ with data_file.open(newline="", encoding="utf-8") as file:
     ]
 
 average_rainfall = sum(rainfall_values) / len(rainfall_values)
+sd = (sum((x - average_rainfall) ** 2 for x in rainfall_values) / len(rainfall_values)) ** 0.5
 
 print(f"Average monthly rainfall: {average_rainfall:.1f} mm")
 
